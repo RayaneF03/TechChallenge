899fc1d9c0efaff2ebdff564f688bdb6920aa0cd
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-07-31 14:57:03 -0300

    M04: CRUD completo de Categoria com validações

--- TechChallenge/obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ TechChallenge/obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5df0794d4acc166efef802a1189c27e424fbd14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a468880961cfd4b0baa5095424edc17f335656")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

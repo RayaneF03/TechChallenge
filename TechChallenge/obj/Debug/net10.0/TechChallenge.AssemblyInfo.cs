a5df0794d4acc166efef802a1189c27e424fbd14
RayaneF03 <rayane.f.santos7@aluno.senai.br>
2026-07-31 10:36:20 -0300

    Refactor code structure for improved readability and maintainability

--- TechChallenge/obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
+++ TechChallenge/obj/Debug/net10.0/TechChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fbb0b638e704e6480079553bf7697a8c8847c8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a04272ec5a2455738dc8eb2ad8dd67da397e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
